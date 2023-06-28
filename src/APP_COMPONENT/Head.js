06ed7875f3bbe48beec14eb787ca7e15988143d2
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-06-28 11:14:55 +0330

    add head component

---
+++ src/APP_COMPONENT/Head.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../CUSTOM-COMPONENT/Container';
+import style from './Head.module.css';
+
+const Head = ()=>{
+    return (
+        <Container style={style['main-head']}>
+
+        </Container>
+    )
+}
+
+
+export default Head;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
-import './App.css';
+import React from 'react';
+import style from  './App.module.css';
+import Container from './CUSTOM-COMPONENT/Container';
+import Head from './APP_COMPONENT/Head';
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
+      <Container style={style['main-container']}>
+        <Head></Head>
+
+      </Container>
 
     </div>
   );
