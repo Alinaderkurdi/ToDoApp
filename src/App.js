373260d68192455107bafc7a6747c9876b982987
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-07-05 16:35:08 +0330

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import style from  './App.module.css';
 import Container from './CUSTOM-COMPONENT/Container';
-import Head from './APP_COMPONENT/Head';
+//import Head from './APP_COMPONENT/Head';
 import Main from './APP_COMPONENT/Mian';
+import SideSection from './APP_COMPONENT/SideSection';
 
 function App() {
   return (
     <div className={style.App}>
       <Container style={style['main-container']}>
-        <Head></Head>
-        <Main></Main>
+        <SideSection />
+        <Main />
       </Container>
-
     </div>
   );
 }
