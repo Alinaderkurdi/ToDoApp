082e72309d5594587c47ce7d95b95ef962cbb98c
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-07-25 15:25:44 +0330

    add new component and modify pages

--- src/APP_COMPONENT/Main_COMPONENT/OutPutCOMPONENT/Pages/AssigendToMe.js
+++ src/APP_COMPONENT/Main_COMPONENT/OutPutCOMPONENT/Pages/AssigendToMe.js
@@ -2,12 +2,14 @@ import React from 'react';
 import style from './PageGlobalStyle.module.css';
 import Container from '../../../../CUSTOM-COMPONENT/Container';
 import SingleToDo from '../SingleToDo';
+import NoneContentPage from './PAGE_COMPONENT/NoneContentPage';
 
 
 const AssigendToMe =()=>{
+    let a = false;
     return(
         <Container style={style['continer-main']}>
-            <SingleToDo></SingleToDo>
+            {a ? <NoneContentPage /> : <SingleToDo />}
         </Container>
     )
 }
