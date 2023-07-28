03d43cae0537b4093cb2414366dc44419a172710
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-07-28 14:27:55 +0330

    add redux toolkit modify store to write store with redux toolkit

---
+++ src/STORE/SLICES/stection-name.js
@@ -0,0 +1,26 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const sectionNameState = {
+    name : 'My Day',
+    sectionLogo : 'bi-brightness-high',
+    iconColor : '#039EDF',
+}
+
+
+const sectionSlice =  createSlice({
+    name: 'section-name',
+    initialState : sectionNameState,
+    reducers : {
+        changePage:(state, action)=>{
+            state.name = action.payload.SectionName
+            state.sectionLogo = action.payload.iconName 
+            state.iconColor = action.payload.iconColor
+        }
+    }
+})
+
+
+
+export const sectionAction = sectionSlice.actions
+export  default sectionSlice.reducer
--- src/STORE/store.js
+++ src/STORE/store.js
@@ -1,52 +1,18 @@
-//import { defaultMethod } from 'react-router-dom/dist/dom';
-import { legacy_createStore } from 'redux';
+
+import { configureStore} from '@reduxjs/toolkit';
+import stectionName from './SLICES/stection-name';
 
 
-const initialState = {
-    stctionName : {
-        name : 'My Day',
-        sectionLogo : 'bi-brightness-high',
-        iconColor : '#039EDF',
-    },
-    colortheme: {
-        mainBackground: '#11100F',
-        seconderyBackground: '#252423',
-        fontColor: '#ffff',
+
+
+
+
+
+
+const store = configureStore({
+    reducer: {
+        section: stectionName
     }
-}
-
-const reduser = ( state= initialState, action)=>{
-
-    if(action.type === 'CHANGE-PAGE'){
-        return {
-            ...state,
-            colortheme :{
-                ...state.colortheme
-            },
-            stctionName : {
-                ...state.stctionName,
-                name : action.payload.name,
-                sectionLogo: action.payload.sectionLogo,
-                iconColor: action.payload.iconColor
-            }
-        }
-    }else if (action.type === 'CHANGE-THEM'){
-        //return{
-        //   ...state,
-        //   stctionName: {
-        //    ...state.stctionName
-        //   },
-        //   colortheme: {
-        //    
-        //   }
-        //}
-    }
-
-    return{
-        ...state 
-    }
-}
-
-const store = legacy_createStore(reduser)
+})
 
 export default store ;
