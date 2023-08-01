42677215efa7cca38fe9bd4abe1e29bf4e3ea83e
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-08-01 15:48:26 +0330

    add more animatiom to container component

--- src/CUSTOM-COMPONENT/Container.js
+++ src/CUSTOM-COMPONENT/Container.js
@@ -1,14 +1,24 @@
  
 import React from 'react';
 import { motion } from 'framer-motion/dist/framer-motion'
+//import { useSelector } from 'react-redux';
+
 
 
 const Container = (props) =>{
     //console.log(props.animation)
+    //console.log(props.inlineStyle)
+   // const themeBackground = useSelector(state => state.colortheme)
+ //   const { inlineStyle , fontColor } = props
+   // const keyName = inlineStyle 
+   // const fontKey = fontColor
+
+  
     return(
         <motion.div className={props.style}
          animate={props.animation}
          initial={props.initial}
+         transition={{duration:props.durationValue}}
         >
         {props.children}
         </motion.div>
